6ae411fe14977542040baea32f3bd7c5dda8ad16
Abdullahi <muxsinxareed6@gmail.com>
2024-06-03 09:35:19 +0200

    Added functions for adding and removing tasks

--- src/scripts/today.js
+++ src/scripts/today.js
@@ -1,30 +1,18 @@
-const taskTypes = document.querySelector('.taskTypes');
+const todayTasks = [];
 
-// allow the user to add a new task
-const addTask = document.createElement('button');
-addTask.classList.add('addTask');
-addTask.textContent = 'Add Task';
-
-function addTaskForm() {
-    const taskForm = document.createElement('form');
-    taskForm.classList.add('taskForm');
-    taskForm.innerHTML = `
-        <label for="taskInput">Title:</label>
-        <input type="text" class="taskInput" placeholder="Add a task...">
-        <label for="taskDescription">Descriptions:</label>
-        <button type="submit" class="submitTask">Submit</button>
-        
-    `;
-
-    taskTypes.appendChild(taskForm);
+class Task {
+    constructor(title, time, description, starred) {
+        this.title = title;
+        this.time = time;
+        this.description = description;
+        this.starred = starred;
+    }
 }
 
-// Append the addTask button to the taskTypes element
-taskTypes.appendChild(addTask);
+function addTaskToToday(userTask) {
+    todayTasks.push(userTask);
+}
 
-export function addTaskBtn(taskType) {
-    taskType.addEventListener('click', () => {
-        // Add the click event listener to the addTask button
-        addTask.addEventListener('click', addTaskForm);
-    });
+function removeTaskFromToday(index) {
+    todayTasks.splice(index, 1);
 }
